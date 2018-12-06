72764e7e91189fa2cc4048518c2266186b7139c9
Dmitry P <dmitry.porozhnyakov@mail.ru>
2018-12-06 23:30:32 +0300

    Renamed DateIntervalTransformer constants and variables

--- src/Date/Interval/DateIntervalTransformer.php
+++ src/Date/Interval/DateIntervalTransformer.php
@@ -9,14 +9,14 @@ use DateTimeImmutable;
 
 final class DateIntervalTransformer
 {
-    private const TIME_POINT = '1970-01-01T00:00:00+00:00';
+    private const UNIX_EPOCH_STARTED_AT = '1970-01-01T00:00:00+00:00';
 
     public static function toSeconds(DateInterval $interval): int
     {
-        $reference = DateTimeImmutable::createFromFormat(DateTime::ATOM, self::TIME_POINT);
-        $endTime = $reference->add($interval);
+        $originalTime = DateTimeImmutable::createFromFormat(DateTime::ATOM, self::UNIX_EPOCH_STARTED_AT);
+        $timeWithInterval = $originalTime->add($interval);
 
-        return $endTime->getTimestamp() - $reference->getTimestamp();
+        return $timeWithInterval->getTimestamp() - $originalTime->getTimestamp();
     }
 
     private function __construct()
